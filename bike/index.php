e76aa177674683b1c6c6ddcd77995b07ccd8e9c1
Ryan <rh0381@gmail.com>
2015-09-23 13:11:55 +0100

    bike list

---
+++ bike/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>404 - Shixma</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/custom_edit_style.css" rel="stylesheet">
+        
+	</head>
+
+	<body>
+<!-- Main -->
+
+		<div class="container">
+<h2>Motorbike</h2>
+  <p>Shixmas motor bike list, will all be purchased after/during July 2016...<br />
+  Smartwater Security  | £24.99 Anually</p>            
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Price</th>
+		<th>Link</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Kawasaki Ninja 300 ABS (with custom parts)</td>
+        <td>£5856.85</td>
+		<th><a href="http://configurator.kawasaki.eu/en-gb/#c=eyJsYW5ndWFnZSI6ImVuLWdiIiwidmVyc2lvbsSKIjEuMcSSImJhc2tldMSKWyIyNThFWFAwMDc2xKEwMzlQQ1XEsjA5xKExOTdCV1PEvTciXcSTxIJ5xJZzxKk3xYoiaW5jbHVkZUJpxKZQcmljxIk6dHJ1ZcSTZsWUYWzEimbFq3PFpyJtb8WZbElExJzErzPEskFFRkEtU0XEoXNwZWNpxatFZGl0xJnEm8SLxbrFvMW%2BxoDGgi1hYsWPxJPEg2dsxaIiRiJ9">Link</a></th>
+      </tr>
+      <tr>
+        <td>Shoei gt-air Helmet</td>
+        <td>£369.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/193632">link</a></th>
+      </tr>
+      <tr>
+        <td>Alpine Stars Jaws Leather Jacket</td>
+        <td>£369.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/199027">Link</a></th>
+      </tr>
+	        <tr>
+        <td>Alpinestars Missile Leather Pants</td>
+        <td>£229.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/199306">link</a></th>
+      </tr>
+	        <tr>
+        <td>Alpinestars Bionic Back Protector</td>
+        <td>£89.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/199635">link</a></th>
+      </tr>
+	  	        <tr>
+        <td>Alpinestars Nucleon KR-C Chest Protector</td>
+        <td>£37.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/236855">link</a></th>
+      </tr>	        <tr>
+        <td>Alpinestars S-MX 5 Boots</td>
+        <td>£109.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/63696">link</a></th>
+      </tr>	        <tr>
+        <td>Alpinestars SP-8 Gloves</td>
+        <td>£57.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/113678">link</a></th>
+      </tr>	        <tr>
+        <td>Kriega US-10 DryBag (Tail bag)</td>
+        <td>£54.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/206954">link</a></th>
+      </tr>	        <tr>
+        <td>Givi EA106 Easy Magnetic Tank Bag</td>
+        <td>£33.29</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/208795">link</a></th>
+      </tr>	        <tr>
+        <td>Art Motorcycle training (all to get a2)</td>
+        <td>£550</td>
+		<th><a href="http://www.artmotorcycletraining.co.uk/a2-motorcycle-licence.htm">link</a></th>
+      </tr>	        <tr>
+        <td>Xena Xi Disc Lock - 6mm pin</td>
+        <td>£19.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/140262">link</a></th>
+      </tr>
+	  <tr>
+        <td>Oxford Loop Cable Lock</td>
+        <td>£21.99</td>
+		<th><a href="http://www.sportsbikeshop.co.uk/motorcycle_parts/content_prod/55846">link</a></th>
+      </tr>
+    </tbody>
+	<h3>Total: £7803.04
+	<br /><small>Insurance: £154.01 monthly</small></h3>
+  </table>
+		</div>
+	
+	
+	<!--More Script-->
+	
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script> $('#myCarousel').carousel({ interval: 4300 }) </script>
+	</body>
+
+</html>
